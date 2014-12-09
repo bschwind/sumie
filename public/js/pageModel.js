821d70d39fd7e826b4d0490a478cfd74392212f7
Brian Schwind <brianmschwind@gmail.com>
2014-12-08 20:54:00 -0500

    Adding some controls to change the brush variables

--- public/js/pageModel.js
+++ public/js/pageModel.js
@@ -3,7 +3,9 @@ define(["knockout"], function(ko) {
 		var self = this;
 
 		self.pageName = ko.observable("墨絵");
-		self.brushRadius = ko.observable(60);
+		self.brushRadius = ko.observable(20);
+		self.bristleWidth = ko.observable(0.2);
+		self.numRandomPoints = ko.observable(400);
 	};
 
 	var pageModel = new PageModel();
